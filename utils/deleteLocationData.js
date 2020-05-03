094930c54e3c81cc4006c86be5c8f83e50a4dd06
Syed-Kashan-Adil <syed.kashan.adil@gmail.com>
2020-05-04 03:37:49 +0500

    fixed cron job logic

--- utils/deleteLocationData.js
+++ utils/deleteLocationData.js
@@ -2,7 +2,7 @@ import { LocationModel } from "../models/location"
 export const deleteLocationData = () => {
     setInterval(async () => {
         const fifteenMinutesOld = new Date()
-        fifteenMinutesOld.setMinutes(fifteenMinutesOld.getMinutes() - 15)
-        return LocationModel.deleteMany({ timestamps: { $gte: fifteenMinutesOld } })
+        fifteenMinutesOld.setMinutes(fifteenMinutesOld.getMinutes() - 5)
+        return LocationModel.deleteMany({ createdAt: { $lte: fifteenMinutesOld } })
     }, 900000)
 }
